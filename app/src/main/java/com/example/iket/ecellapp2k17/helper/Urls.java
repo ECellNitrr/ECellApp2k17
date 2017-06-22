1c10ee3625cf2db8b060b34ebbf3f472e64f276d
Vrihas123 <vrihas007@gmail.com>
2017-06-22 12:15:40 +0530

    helper

---
+++ app/src/main/java/com/example/iket/ecellapp2k17/helper/Urls.java
@@ -0,0 +1,11 @@
+package com.example.iket.ecellapp2k17.helper;
+
+/**
+ * Created by vrihas on 6/21/2017.
+ */
+//All the Urls will be here in this class..!!
+public class Urls {
+
+    public static final String BASE_URL = "";
+    public static final String REQUEST_BLOGS = "blogs/";
+}
